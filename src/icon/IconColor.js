26e3183fcc172026d25f8d1029ad996d379ad8ee
Kenny-QD <KennyQDang@gmail.com>
2020-07-11 13:23:46 -0500

    created icon, styling

---
+++ src/icon/IconColor.js
@@ -0,0 +1,14 @@
+ const tintColor = '#9C4C33';
+
+export default {
+  tintColor,
+  tabIconDefault: '#94A480',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
